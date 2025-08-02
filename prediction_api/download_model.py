a1ef4b61b7518e46c318b1a2a35a684865ce1cf0
selhia <sbendjennad@gmail.com>
2025-08-02 05:03:16 -0700

    creation dossier model

--- prediction_api/download_model.py
+++ prediction_api/download_model.py
@@ -1,7 +1,11 @@
 import requests
+import os
 
 url = "https://github.com/Selhia/image-segmentation-app/raw/main/image-segmentation-app/model/unet_resnet50_cityscapes.tflite"
 output_path = "prediction_api/model/unet_resnet50_cityscapes.tflite"
+
+# Crée le dossier model s'il n'existe pas
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
 response = requests.get(url)
 with open(output_path, "wb") as f:
